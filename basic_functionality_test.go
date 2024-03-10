09dadb3b85151b3efb81618612f22bb01b011262
Alexey Ershov <alexa3mail@gmail.com>
2024-03-10 18:48:29 +0300

    Add basic functionality testing

---
+++ basic_functionality_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	discord "github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func TestBasicFunctionality(t *testing.T) {
+
+	// Load env. variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("[INFO] No .env file found.")
+	}
+	if os.Getenv("TESTING_CLIENT_TOKEN") == "" {
+		log.Fatalln("[FATAL] TESTING_CLIENT_TOKEN env. variable not specified.")
+	}
+	if os.Getenv("TESTING_CLIENT_ID") == "" {
+		log.Fatalln("[FATAL] TESTING_CLIENT_ID env. variable not specified.")
+	}
+
+	s := SessionWrapper{initNewDiscordSession(os.Getenv("TESTING_CLIENT_TOKEN"))}
+
+	s.Identify.Intents = discord.IntentsAll
+
+	s.StateEnabled = true
+	s.State.MaxMessageCount = 10
+
+	s.addEventHandlers()
+
+	s.addCommandHandlers()
+
+	s.openConnection()
+
+	s.registerCommands(os.Getenv("TESTING_GUILD_ID"))
+
+	s.removeRegisteredCommands(os.Getenv("TESTING_CLIENT_ID"), os.Getenv("TESTING_GUILD_ID"))
+
+	s.closeConnection()
+}
--- main.go
+++ main.go
@@ -16,6 +16,10 @@ import (
 	"github.com/chilipizdrick/schizgophrenia-got/events"
 )
 
+type SessionWrapper struct {
+	*discord.Session
+}
+
 func init() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -33,10 +37,7 @@ func init() {
 
 func main() {
 	// Initialise new discord bot session
-	s, err := discord.New("Bot " + os.Getenv("CLIENT_TOKEN"))
-	if err != nil {
-		log.Fatalln("[FATAL] Could not create Discord Bot session.", err)
-	}
+	s := SessionWrapper{initNewDiscordSession(os.Getenv("CLIENT_TOKEN"))}
 
 	// Set intents
 	s.Identify.Intents = discord.IntentsAll
@@ -45,32 +46,65 @@ func main() {
 	s.StateEnabled = true
 	s.State.MaxMessageCount = 10
 
-	// Add event handlers
+	s.addEventHandlers()
+
+	s.addCommandHandlers()
+
+	s.openConnection()
+
+	s.registerCommands(os.Getenv("GUILD_ID"))
+
+	// Close connection when process exits
+	defer func() {
+		s.closeConnection()
+	}()
+
+	// Wait here until CTRL-C or other term signal is received
+	fmt.Println("Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	s.removeRegisteredCommands(os.Getenv("CLIENT_ID"), os.Getenv("GUILD_ID"))
+}
+
+func initNewDiscordSession(clientID string) *discord.Session {
+	s, err := discord.New("Bot " + clientID)
+	if err != nil {
+		log.Fatalln("[FATAL] Could not create Discord Bot session.", err)
+	}
+
+	return s
+}
+
+func (s *SessionWrapper) addEventHandlers() {
 	log.Println("[INFO] Adding event handlers...")
 	for _, eh := range events.EventHandlers {
 		s.AddHandler(eh)
 	}
+}
 
-	// Add command handlers
+func (s *SessionWrapper) addCommandHandlers() {
 	log.Println("[INFO] Adding command handlers...")
 	s.AddHandler(func(s *discord.Session, i *discord.InteractionCreate) {
 		if c, ok := commands.SlashCommands[i.ApplicationCommandData().Name]; ok {
 			c.CommandHandler(s, i)
 		}
 	})
+}
 
-	// Open connection
-	err = s.Open()
+func (s *SessionWrapper) openConnection() {
+	err := s.Open()
 	if err != nil {
 		log.Fatalln("[FATAL] Could not open connection.", err)
 	}
+}
 
-	// Register commands
+func (s *SessionWrapper) registerCommands(guildID string) {
 	registerCommandsFlag := strings.ToLower(os.Getenv("REGISTER_COMMANDS"))
 	if funk.Contains([]string{"1", "yes", "on", "true"}, registerCommandsFlag) {
 		log.Println("[INFO] Registering commands...")
 		var registeredCommands []*discord.ApplicationCommand
-		guildID := os.Getenv("GUILD_ID")
 		for _, v := range commands.SlashCommands {
 			cmd, err := s.ApplicationCommandCreate(s.State.User.ID, guildID, v.CommandData)
 			if err != nil {
@@ -83,36 +117,30 @@ func main() {
 			log.Printf("[INFO] /%v", v.Name)
 		}
 	}
+}
 
-	// Close connection when process exits
-	defer func() {
-		log.Println("[INFO] Closing connection...")
-		err := s.Close()
-		if err != nil {
-			log.Fatalln("[FATAL] Failed to close connection.", err)
-		}
-		log.Println("[INFO] Conneciton successfuly closed.")
-	}()
+func (s *SessionWrapper) closeConnection() {
+	log.Println("[INFO] Closing connection...")
+	err := s.Close()
+	if err != nil {
+		log.Fatalln("[FATAL] Failed to close connection.", err)
+	}
+	log.Println("[INFO] Conneciton successfuly closed.")
 
-	// Wait here until CTRL-C or other term signal is received
-	fmt.Println("Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
+}
 
-	// Remove registered commands
+func (s *SessionWrapper) removeRegisteredCommands(clientID string, guildID string) {
 	removeCommandsFlag := strings.ToLower(os.Getenv("REMOVE_COMMANDS"))
 	if funk.Contains([]string{"1", "yes", "on", "true"}, removeCommandsFlag) {
 		log.Println("[INFO] Removing commands...")
 
-		registeredCommands, err := s.ApplicationCommands(os.Getenv("CLIENT_ID"), os.Getenv("GUILD_ID"))
+		registeredCommands, err := s.ApplicationCommands(clientID, guildID)
 		if err != nil {
 			log.Panicf("[ERROR] Could not fetch registered commands: %v", err)
 			return
 		}
 
 		for _, v := range registeredCommands {
-			guildID := os.Getenv("GUILD_ID")
 			err := s.ApplicationCommandDelete(s.State.User.ID, guildID, v.ID)
 			if err != nil {
 				log.Panicf("[ERROR] Could not delete '%v' command: %v", v.Name, err)
